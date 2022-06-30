db3e3791c28422a125992a292f11da1b602b9939
Ximena Gonzalez <xigonzalez28@gmail.com>
2022-06-30 13:53:44 +0200

    afficher erreurs a partir de la terminal

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,24 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+
 
 class ContactController extends AbstractController
 {
     #[Route('/contact/create', name: 'contact.create')]
     public function create(): Response
     {
-        $data=[
-            "nom"=>'',
-            "prenom"=>'',
-            "email"=>'',
-            "message"=>'',
-        ];
-        return $this->render('contact/create.html.twig', [
-            'data' => $data,
-        ]);
+       
+        return $this->render('contact/create.html.twig');
     }
-   
+    #[Route('/contact', name: 'contact.store', methods:['POST'])]
+    public function store(Request $request): Response
+    {
+    // dd($request->get('nom'));
+    $contact= new Contact();//click control+space pour creer une use
+    $contact->setNom($request->get('nom'))
+            ->setPrenom($request->get('prenom'))
+            ->setEmail($request->get('email'))
+            ->setMessage($request->get('message'));
+
+     return $this->render('contact/create.html.twig');
+    }
 }
