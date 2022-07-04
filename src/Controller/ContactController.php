1314c4f98643e65f15e2af1880f32f936d8a6412
Ximena Gonzalez <xigonzalez28@gmail.com>
2022-07-04 17:01:19 +0200

    Delete et edit API

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Repository\ContactRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,11 +13,17 @@ use App\Form\ContactType;
 
 
 class ContactController extends AbstractController
-{
-    #[Route('/contact', name: 'contact.index', methods:['GET'])]
-    public function index(ContactRepository $repo)
+{   
+    private ContactRepository $repo;
+    public function __construct(ContactRepository $repo)
     {
-        $contacts = $repo->findAll();
+        $this->repo=$repo;
+    }
+
+    #[Route('/contact', name: 'contact.index', methods: ['GET'])]
+    public function index()
+    {
+        $contacts = $this->repo->findAll();
 
         return $this->render('contact/index.html.twig', [
             'contacts' => $contacts
@@ -26,45 +31,80 @@ class ContactController extends AbstractController
     }
 
 
-    #[Route('/contact/create', name: 'contact.create')]
-    public function create(Request $request, EntityManagerInterface $em): Response
-    {  
-        $contact= new Contact();
-        $form=$this->createForm(ContactType::class, $contact);
-        $form->handleRequest($request);
+    #[Route('/contact/create', name: 'contact.create', methods:['GET', 'POST'])]
+    public function create(Request $request): Response
+    {
+        $contact = new Contact();
 
-        if($form->isSubmitted()&& $form->isValid()){
-            $em->persist($contact);
-            $em->flush();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+        // dd($form->getData());
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->repo->add($contact, true);
+            
             return $this->redirect('/contact');
         }
-      
+
         return $this->render('contact/create.html.twig', [
-            'form'=>$form->createView()//view permet de générer la vue du formulaire
+            'form' => $form->createView() //view permet de générer la vue du formulaire
         ]);
         // return $this->render('contact/create.html.twig');
     }
 
 
-    #[Route('/contact', name: 'contact.store', methods: ['POST'])]
-    public function store(Request $request, EntityManagerInterface $em): Response //injection de depandance d'entity manager
-    {
-        // dd($request->get('nom'));
-        $contact = new Contact(); //click control+space pour creer une use
-        $contact->setNom($request->get('nom'))
-            ->setPrenom($request->get('prenom'))
-            ->setEmail($request->get('email'))
-            ->setMessage($request->get('message'));
-        //    dd($contact);
+    // #[Route('/contact', name: 'contact.store', methods: ['POST'])]
+    // public function store(Request $request, EntityManagerInterface $em): Response //injection de depandance d'entity manager
+    // {
+    //     // dd($request->get('nom'));
+    //     $contact = new Contact(); //click control+space pour creer une use
+    //     $contact->setNom($request->get('nom'))
+    //         ->setPrenom($request->get('prenom'))
+    //         ->setEmail($request->get('email'))
+    //         ->setMessage($request->get('message'));
+    //     //    dd($contact);
 
-        $em->persist($contact); //il dit a l'objet de faire un instance de le preparerle objet el le sauvegarder dans la base de donnes de $contact
-        $em->flush(); //enregistrer
-        return $this->render('contact/create.html.twig');
+    //     $em->persist($contact); //il dit a l'objet de faire un instance de le preparerle objet el le sauvegarder dans la base de donnes de $contact
+    //     $em->flush(); //enregistrer
+    //     return $this->render('contact/create.html.twig');
+    // }
+
+
+    #[Route('/contact/{id}', name: 'contact.show', methods: ['GET'])]
+    public function show($id): Response
+    {
+        $contact = $this->repo->find($id);
+        // dd($contact);
+        return $this->render('contact/show.html.twig', [
+            'contact' => $contact
+        ]);
     }
 
-    #[Route('/contact/{id}', name: 'contact.show', methods:['GET'])]
-    public function show( $id, ContactRepository $repo): Response
+
+    #[Route('/contact/{id}', name: 'contact.delete', methods: ['POST'])]
+    public function delete($id): Response
     {
-        $contact= $repo->findOne($id);
+        $contact = $this->repo->find($id);
+        $this->repo->remove($contact, true);
+
+        return $this->redirect('/contact');
     }
+
+    // modifier le form
+    #[Route('/contact/edit/{id}', name: 'contact.edit', methods: ['GET', 'POST'])]
+    public function edit($id, Request $request): Response
+    {    
+        $contact= $this->repo->find($id);
+        $form= $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if($form->isSubmited() && $form->isValid()){
+            $this->repo
+        }
+
+
+        return $this->render('/contact/edit.html.twig',
+        ['form' => $form->createView()]);
+    }
+        
 }
+
--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -14,35 +14,34 @@ class Contact
     #[ORM\Column(type: 'integer')]
     private $id;
 
+
     /**
      * @Assert\NotBlank (message="Ce champ ne peut être vide.")
      */
-
     #[ORM\Column(type: 'string', length: 255)]
     private $Nom;
+
+
     /**
      * @Assert\NotBlank (message="Ce champ ne peut être vide.")
      */
-
     #[ORM\Column(type: 'string', length: 255)]
     private $Prenom;
+    
     /**
      * @Assert\NotBlank (message="Ce champ ne peut être vide.")
      */
-
-
     #[ORM\Column(type: 'string', length: 255)]
     private $Email;
+
+
       /**
      * @Assert\NotBlank (message="Ce champ ne peut être vide.")
      */
-
     #[ORM\Column(type: 'text')]
     private $Message;
-      /**
-     * @Assert\NotBlank
-     */
-
+     
+   
     public function getId(): ?int
     {
         return $this->id;
